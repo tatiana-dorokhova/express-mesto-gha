668b71758a81ebc2da8d26f7636bd1f0383371b6
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-01-21 10:55:51 +0300

    13PR улучшения после первого ревью

--- routes/cards.js
+++ routes/cards.js
@@ -7,14 +7,14 @@ const {
   dislikeCard,
 } = require("../controllers/cards");
 
-router.get("/cards", getCards); // возвращает все карточки
+router.get("/", getCards); // возвращает все карточки
 
-router.delete("/cards/:cardId", deleteCardById); // удаляет карточку по идентификатору
+router.delete("/:cardId", deleteCardById); // удаляет карточку по идентификатору
 
-router.post("/cards", createCard); // создаёт карточку
+router.post("/", createCard); // создаёт карточку
 
-router.put("/cards/:cardId/likes", likeCard); // поставить лайк карточке
+router.put("/:cardId/likes", likeCard); // поставить лайк карточке
 
-router.delete("/cards/:cardId/likes", dislikeCard); // убрать лайк с карточки
+router.delete("/:cardId/likes", dislikeCard); // убрать лайк с карточки
 
 module.exports = router;
