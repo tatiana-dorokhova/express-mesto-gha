4390fbb0c998f04782b183afef544157ea5d41ca
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-01-31 19:30:59 +0300

    14PR исправлен контроллер signin

--- controllers/users.js
+++ controllers/users.js
@@ -168,7 +168,7 @@ const login = (req, res, next) => {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
         })
-        .end();
+        .send({ message: "Authorization successful" });
     })
     .catch(next);
 };
