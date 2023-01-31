b8c143055c2c832d082a9385565fab9f15cc2866
Tatiana Dorokhova <tatjana.dorokhova@yandex.ru>
2023-01-31 16:06:11 +0300

    14PR удалена регулярка для пароля, она включала не все возможные символы и не была необходима

--- utils/constants.js
+++ utils/constants.js
@@ -3,7 +3,6 @@ const INT_SERV_ERR_MESSAGE = "На сервере произошла ошибк�
 
 const JWT_SALT = "q7jb40keq1";
 
-const REGEX_PASSWORD_PATTERN = /^[a-zA-Z0-9]{8,}$/;
 const REGEX_URL_PATTERN =
   /https?:\/\/(w{3}\.)?[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([#]*)/;
 
@@ -11,6 +10,5 @@ module.exports = {
   INTERNAL_SERVER_ERROR,
   INT_SERV_ERR_MESSAGE,
   JWT_SALT,
-  REGEX_PASSWORD_PATTERN,
   REGEX_URL_PATTERN,
 };
